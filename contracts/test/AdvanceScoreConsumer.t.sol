76b681c164fc66d268ef129a1c85db6d0cb2bdbc
Victor .P. Ogbonna <115187256+OutstandingVick@users.noreply.github.com>
2026-09-11 12:38:32 +0100

    test(interface): prove a third-party consumer can request scores

---
+++ contracts/test/AdvanceScoreConsumer.t.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.30;
+
+import {AdvanceTestBase} from "./AdvanceTestBase.t.sol";
+import {IAdvance} from "../src/interfaces/IAdvance.sol";
+import {AdvanceScoreConsumer} from "../src/examples/AdvanceScoreConsumer.sol";
+
+contract AdvanceScoreConsumerTest is AdvanceTestBase {
+    AdvanceScoreConsumer internal consumer;
+
+    function setUp() public override {
+        super.setUp();
+        consumer = new AdvanceScoreConsumer(IAdvance(address(advance)));
+    }
+
+    function testConsumerRequestsAndReadsCurrentScore() public {
+        bytes32 sessionId = consumer.requestScore(_grant(address(consumer)));
+        assertEq(consumer.currentScore(sessionId), advance.computeScore(WALLET));
+    }
+}
